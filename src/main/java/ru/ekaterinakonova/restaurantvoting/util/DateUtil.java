ff4e9e86a50efd7e761ce5ad4719e360d81b9747
Ekaterina-Konova <konovakate1301@gmail.com>
2024-11-20 17:10:20 +0300

    added spring-mvc

---
+++ src/main/java/ru/ekaterinakonova/restaurantvoting/util/DateUtil.java
@@ -0,0 +1,14 @@
+package ru.ekaterinakonova.restaurantvoting.util;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+
+public class DateUtil {
+    private DateUtil() {
+    }
+    public static LocalDate parseLocalDate(@Nullable String str){
+        return StringUtils.isEmpty(str)? null: LocalDate.parse(str);
+    }
+}
---
+++ src/main/java/ru/ekaterinakonova/restaurantvoting/web/converter/DateFormatters.java
@@ -0,0 +1,24 @@
+package ru.ekaterinakonova.restaurantvoting.web.converter;
+
+import org.springframework.format.Formatter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static ru.ekaterinakonova.restaurantvoting.util.DateUtil.parseLocalDate;
+
+public class DateFormatters {
+    private DateFormatters() {
+    }
+    public static class LocalDateFormatter implements Formatter<LocalDateFormatter> {
+        @Override
+        public LocalDate parse(String text, Locale locale) {
+            return parseLocalDate(text);
+        }
+        @Override
+        public String print(LocalDate lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+    }
+}
